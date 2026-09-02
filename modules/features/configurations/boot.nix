a4c694f9c42863e794f26844be00edccd3401821
aaronv <41397746+aaron70@users.noreply.github.com>
2026-09-01 21:26:46 -0600

    Nix flake update and fix boot package

--- modules/features/configurations/boot.nix
+++ modules/features/configurations/boot.nix
@@ -21,7 +21,22 @@ with lib; {
             "udev.log_priority=3"
           ];
           kernelModules = ["ddcci-backlight"];
-          kernelPackages = pkgs.linuxPackages_latest;
+          # TODO: (revert) kernel 7.2 removed strncpy(), breaking ddcci-driver.
+          # Tracked upstream: https://github.com/NixOS/nixpkgs/issues/554041
+          # Fix PR (open, unmerged): https://github.com/NixOS/nixpkgs/pull/556080
+          # Once merged, drop this `extend` override and use:
+          #   kernelPackages = pkgs.linuxPackages_latest;
+          kernelPackages = pkgs.linuxPackages_latest.extend (final: prev: {
+            ddcci-driver = prev.ddcci-driver.overrideAttrs (oldAttrs: {
+              patches = [
+                (pkgs.fetchpatch {
+                  name = "ddcci-sysfs-emit-kernel-7.2.patch";
+                  url = "https://gitlab.com/liquidnya/ddcci-driver-linux/-/commit/9510aa4aebf32678884f55ae251e54012a354ed1.patch";
+                  hash = "sha256-s12ers7nPFaHOB+8/S8t3dtdoR6slukkfNPdghgftNs=";
+                })
+              ] ++ (oldAttrs.patches or []);
+            });
+          });
           extraModulePackages = with config.boot.kernelPackages; [ddcci-driver];
 
           loader.systemd-boot = {

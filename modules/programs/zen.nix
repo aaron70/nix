e154e8dbad9026e185f20126835cbd2b4eb82af5
aaronv <41397746+aaron70@users.noreply.github.com>
2026-09-24 20:41:42 -0600

    feat(programs/zen): Add custom settings to zen (#23)

--- modules/programs/zen.nix
+++ modules/programs/zen.nix
@@ -1,5 +1,31 @@
 {inputs, ...}: {
   anvil.programs.zen = {
     getPackage = {pkgs, ...}: inputs.zen-browser.packages.${pkgs.stdenv.hostPlatform.system}.default;
+    home = {...}: {
+      imports = [
+        inputs.zen-browser.homeModules.beta
+      ];
+
+      programs.zen-browser = {
+        enable = true;
+        setAsDefaultBrowser = true;
+
+        policies = {
+          DisableAppUpdate = true;
+          DisableFeedbackCommands = true;
+          DisableFirefoxStudies = true;
+          DisablePocket = true;
+          DisableTelemetry = true;
+          DontCheckDefaultBrowser = true;
+        };
+
+        profiles.default.settings = {
+          "zen.workspaces.continue-where-left-off" = true;
+          "zen.view.compact.hide-tabbar" = true;
+          "zen.urlbar.behavior" = "float";
+          "zen.welcome-screen.seen" = true;
+        };
+      };
+    };
   };
 }
